60fe95586409a34388b0441cf32bf2680592210e
youv22 <156547771+youv22@users.noreply.github.com>
2024-01-17 11:08:15 +0400

    website.py

--- website.py
+++ website.py
@@ -90,7 +90,7 @@ url = st.text_input('Enter the URL')
 # check the url is valid or not
 if st.button('Check!'):
     try:
-             start_time = time.time()  # Record the start time
+         start_time = time.time()  # Record the start time
         response = re.get(url, verify=False, timeout=4)
         if response.status_code != 200:
             print(". HTTP connection was not successful for the URL: ", url)
